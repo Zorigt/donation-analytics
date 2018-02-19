5c1273021e2a4e2814fdd87e88e4b504c9e79ee1
Zorigt Baz <zorigt.baz@nomissolutions.com>
2018-02-18 16:58:33 -0800

    created zipcode and year arrays

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
 
     percentile = getPercentile(args.INPUT_PERCENTILE)
 
-    alpha_num_array = [[0 for x in range(36)] for y in range(9)] # [A-Z] and [0-9]
-    num_array = [[0 for x in range(10)] for y in range(9)] # [0-9]
+    year_array = [[0 for x in range(10)] for y in range(4)] # [A-Z] and [0-9]
+    zipcode_array = [[0 for x in range(10)] for y in range(5)] # [0-9]
     name_array = [] # item string type contains [A-Z] and comma and space
 
     with open(os.path.join(os.pardir, args.INPUT_ITCONT), "r") as donations:
@@ -40,11 +40,11 @@ if __name__ == "__main__":
                 if recipient and zipcode and year and name and amount:
                     concise_donation_array = [recipient, zipcode, year, name]
                     # found_recipient is not necessary
-                    found_recipient, found_zipcode_year, found_name, found_prev_yr_don = check_donation.checkDonation(concise_donation_array, alpha_num_array, num_array, name_array)
-                    if found_recipient != '' and found_zipcode_year != '' and found_name != '' and found_prev_yr_don != '':
-                        print(found_recipient, found_zipcode_year, found_name, found_prev_yr_don)
-                        temp = recipient_zipcode_year.RecipientZipcodeYear(found_recipient)
-                        a = found_recipient + found_zipcode_year
+                    found_zipcode, found_name, found_prev_yr_don = check_donation.checkDonation(concise_donation_array, zipcode_array, year_array, name_array)
+                    if found_zipcode != '' and found_name != '' and found_prev_yr_don != '':
+                        print(found_zipcode, found_name, found_prev_yr_don)
+                        temp = recipient_zipcode_year.RecipientZipcodeYear(found_zipcode)
+                        a = recipient + found_zipcode
                         print('the object =',a)
                         if a != '':
                             exec(a + "= temp")
--- src/helpers/check_donation.py
+++ src/helpers/check_donation.py
@@ -1,28 +1,16 @@
 import validate_value
 from bisect import bisect_left
 
-def checkDonation(concise_donation_array, alpha_num_array, num_array, name_array):
-    recipient = concise_donation_array[0]
+def checkDonation(concise_donation_array, zipcode_array, year_array, name_array):
     zipcode = concise_donation_array[1]
     year = concise_donation_array[2]
     name = concise_donation_array[3]
 
-    track_recipient = 0 # return 9 if found otherwise not found. There are 9 char counts for found recipient
     track_name = 0 # return 1 if found and 0 for not found. bisection search index found from the name
-    track_zipcode_year = 0 # return 9 if found otherwise not found. There are 9 counts for found zipcode (5 chars) and year (4 chars)
+    track_zipcode = 0 # return 5 if found otherwise not found. There are 5 counts for found zipcode (5 chars)
+    track_year = 0
     track_prev_yr_don = 0 # return 4 if previous year donation found, otherwise not found
 
-
-    # check recipient value
-    for i, char in enumerate(recipient):
-        # 48 is '0' and 57 is '9'
-        if ord(char) >= 48 and ord(char) <= 57:
-            track_recipient += alpha_num_array[i][ord(char)-48]
-            alpha_num_array[i][ord(char)-48] = 1
-        # 65 is 'A' and 90 is 'Z'
-        elif ord(char) >= 65 and ord(char) <= 90:
-            track_recipient += alpha_num_array[i][ord(char) - 55]
-            alpha_num_array[i][ord(char) - 55] = 1
 
     # check name value in name_array
     i = bisect_left(name_array, name)
@@ -35,26 +23,32 @@ def checkDonation(concise_donation_array, alpha_num_array, num_array, name_array
     else:
         track_name = 1
 
-    # check zipcode and year values together
-    zipcode_year = zipcode + year
-    for i, char in enumerate(zipcode_year):
+    # check zipcode
+    for i, char in enumerate(zipcode):
         # 48 is '0' and 57 is '9'
         if ord(char) >= 48 and ord(char) <= 57:
-            track_zipcode_year += num_array[i][ord(char) - 48]
-            num_array[i][ord(char) - 48] = 1
+            track_zipcode += zipcode_array[i][ord(char) - 48]
+            zipcode_array[i][ord(char) - 48] = 1
 
+    # check year
+    for i, char in enumerate(year):
+        # 48 is '0' and 57 is '9'
+        if ord(char) >= 48 and ord(char) <= 57:
+            track_year += year_array[i][ord(char) - 48]
+            year_array[i][ord(char) - 48] = 1
+
+    # if name already exisits then check if they made donations in the previous year
     if track_name == 1:
         prev_year = str(int(year) - 1)
         prev_year_array = [int(prev_year[:1]),int(prev_year[1:2]),int(prev_year[2:3]),int(prev_year[3:])] # split the year digits
-        x = 5 # 4 digit year index starts at 5 and ends at 9
+        x = 0
         for y in prev_year_array:
-            track_prev_yr_don += num_array[x][y]
+            track_prev_yr_don += year_array[x][y]
             x+=1
 
-
-    found_recipient = recipient if track_recipient == 9 else ''
     found_name = name if track_name == 1 else ''
-    found_zipcode_year = zipcode_year if track_zipcode_year == 9 else ''
+    found_zipcode = zipcode if track_zipcode == 5 else ''
     found_prev_yr_don = prev_year if track_prev_yr_don == 4 else ''
-    print(track_recipient,track_zipcode_year, track_name, track_prev_yr_don)
-    return [found_recipient, found_zipcode_year, found_name, found_prev_yr_don]
+
+    print(track_zipcode, track_name, track_prev_yr_don)
+    return [found_zipcode, found_name, found_prev_yr_don]
